319d460633bfdd6f8ba950a6f3e9a8c6fd7cbdb4
Arun <99261603+Arunitdy@users.noreply.github.com>
2025-01-05 17:37:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 //line rendering
-let time=3000;
+
 function typeWriter() {
     const lines = document.querySelectorAll('.poem-line');
     let lineIndex = 0;
@@ -55,7 +55,7 @@ function response(event){
    // sendmail(result);
     if(result.includes("yes")){
         //fall 
-       time=250;
+       setInterval(createSnowflake, 250);
 
        console.log('maile');
        // Create a new div for the mail section
@@ -130,4 +130,4 @@ function createSnowflake() {
         snowflake.remove();
     }, 9000);
 }
-setInterval(createSnowflake, time);
+setInterval(createSnowflake, 3500);
