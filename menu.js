7a66121ae699ada5bf389868b2a5fd80c7b04223
Arun <arunmundakkal003@gmail.com>
2025-03-04 21:11:18 +0530

    Refactor menu hover and active styles for improved interaction

--- menu.js
+++ menu.js
@@ -1,27 +1,13 @@
 console.log("menu");
 
-const menu = document.querySelector(".menu");
-menu.addEventListener("click", menuPopUp);
+// Toggle the sliding menu
+function toggleMenu() {
+    const menu = document.getElementById("menu");
+    const menuButton = document.querySelector(".menu-toggle-button-close");
+    menu.classList.toggle("menu-open");
 
-//menu from left side
-
-function menuPopUp() {
-    console.log("menuPopUp start");
-    const menuDiv = document.createElement("div");
-    menuDiv.classList.add("menuDiv");
-    document.querySelector(".m").appendChild(menuDiv);
-    const menuCloseButton = document.createElement("button");
-    menuCloseButton.classList.add("menuCloseButton");
-    menuCloseButton.innerHTML = "&times";
-    menuCloseButton.onclick = closeMenu;
-    menuDiv.appendChild(menuCloseButton);
-
-    const menuList = document.createElement("ul");
-    menuList.classList.add("menuList");
-}
-
-function closeMenu() {
-    const menuDiv = document.querySelector(".menuDiv");
-    menuDiv.remove();
-    console.log("Menu section closed");
+    if (menuButton) {
+        console.log("close");
+        menuButton.classList.toggle("menu-toggle-button-close");
+    }
 }
